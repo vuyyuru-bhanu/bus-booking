ebf4a0ce520bade39d88ca82981452fc907087ae
bhanu <prasad.bhanu59@gmail.com>
2024-09-18 12:21:59 +0530

    test

--- bus-booking-frontend/src/app/app.module.ts
+++ bus-booking-frontend/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Import this if you're using ngModel
 import { AppComponent } from './app.component';
+import { BookingComponent } from './booking/booking.component';
+import { BusSearchComponent } from './bus-search/bus-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // other components
+    BookingComponent, // Add your components here
+    BusSearchComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, // Ensure FormsModule is imported for ngModel
     // other modules
   ],
   providers: [],
