449e2308020b64c807cabeae2573fe0efd6b3662
bhanu <prasad.bhanu59@gmail.com>
2025-07-12 23:34:40 +0530

    test

--- backend/controllers/payment_controller.go
+++ backend/controllers/payment_controller.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"net/http"
 
-	"bus-booking/utils"
 	"github.com/gin-gonic/gin"
 )
 
--- backend/controllers/seat_controller.go
+++ backend/controllers/seat_controller.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"net/http"
 	"strconv"
-	"strconv"
 
 	"bus-booking/config"
 	"bus-booking/models"
